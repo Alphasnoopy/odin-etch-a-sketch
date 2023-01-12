236f183c4740a9b26c488a3bcc35482710b73537
Elita Chan <elitachan03@gmail.com>
2023-01-11 22:42:12 -0500

    Add switch case for each button except reset

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ const slider = document.getElementById('slider');
 
 const canvas = document.getElementById('canvas');
 const slider_Label = document.getElementById('sizeLabel');
+const buttons = document.querySelector("#buttons").childNodes;
 let curr_Size = 0;
 let div_Num = 1;
 let corner_List = [];
+let color_Style = "";
 
 function colorChange(e){
-    e.target.style.backgroundColor = document.getElementById("colorPicker").value
+    switch(color_Style) {
+        case "black":
+            e.target.style.backgroundColor = "rgb(0, 0, 0)";
+            break;
+        case "shade":
+            let rgbColor = e.target.style.backgroundColor;
+            let rgbArr = rgbColor.substring(4, rgbColor.length-1).split(',').map(function (value){
+                return value.trim() - 25.5;
+            });
+            e.target.style.backgroundColor = `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`;
+            break;
+        case "rainbow":
+            let red_Color = Math.random() * 255;
+            let green_Color = Math.random() * 255;
+            let blue_Color = Math.random() * 255;
+            e.target.style.backgroundColor = `rgb(${red_Color}, ${green_Color}, ${blue_Color})`;
+            break;
+        default:
+            e.target.style.backgroundColor = document.getElementById("colorPicker").value;
+    }
 }
 
 function createGrid(){
@@ -37,7 +58,8 @@ function createGrid(){
         for (let i = 0; i < grid_Diff; i++) {
             let div = document.createElement('div');  
             div.classList.add('pixel'); 
-            div.setAttribute('id', div_Num); 
+            div.setAttribute('id', div_Num);
+            div.style.backgroundColor =  "rgb(206, 201, 201)"; 
             canvas.appendChild(div);
             div.addEventListener('mouseover', colorChange);
             div_Num += 1;
@@ -56,7 +78,6 @@ function createGrid(){
     top_Right.style.cssText = 'border-top-right-radius: 1em;';
     bottom_Left.style.cssText = 'border-bottom-left-radius: 1em;';
     curr_Size = gridSize;
-
 }
 
 function sketchStart() {
@@ -66,6 +87,11 @@ function sketchStart() {
         slider_Label.textContent = gridSize;
     });
     slider.addEventListener("change", createGrid);
+    buttons.forEach(button => {
+        button.addEventListener('mousedown', () => {
+            color_Style = button.id;
+        })
+    })
 }
 
 sketchStart()
