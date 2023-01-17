0e9a2d62064ee83886ccad7fcaa3769646a49bac
Elita Chan <elitachan03@gmail.com>
2023-01-17 00:38:29 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const slider = document.getElementById('slider');
-
 const canvas = document.getElementById('canvas');
 const slider_Label = document.getElementById('sizeLabel');
 const buttons = document.querySelector("#buttons").childNodes;
@@ -8,6 +7,7 @@ let div_Num = 1;
 let corner_List = [];
 let color_Style = "";
 
+// Iterate through each pixel to change back to orginal color
 function clearCanvas(){
     let pixel_List;
     pixel_List = document.querySelectorAll(".pixel");
@@ -17,11 +17,13 @@ function clearCanvas(){
     });
 }
 
+// Color used when hovering over pixel
 function colorChange(e){
     switch(color_Style) {
         case "black":
             e.target.style.backgroundColor = "rgb(0, 0, 0)";
             break;
+        // Get each rgb value and increase by 10% of 255 (or 25.5)
         case "shade":
             let rgbColor = e.target.style.backgroundColor;
             let rgbArr = rgbColor.substring(4, rgbColor.length-1).split(',').map(function (value){
@@ -29,6 +31,7 @@ function colorChange(e){
             });
             e.target.style.backgroundColor = `rgb(${rgbArr[0]}, ${rgbArr[1]}, ${rgbArr[2]})`;
             break;
+        // Each rgb value is randomly generated using number between 0 and 1 multiplied by 255
         case "rainbow":
             let red_Color = Math.random() * 255;
             let green_Color = Math.random() * 255;
@@ -45,8 +48,10 @@ function createGrid(){
     let top_Right;
     let grid_Diff = 0;
     const gridSize = Number(slider.value);
+    // Set css variable
     canvas.style.setProperty('--grid-size', gridSize);
 
+    // Remove corner pixel radius from previous grid
     if (corner_List.length > 0) {
         bottom_Left = document.getElementById(String(corner_List.pop()));
         top_Right = document.getElementById(String(corner_List.pop()));
@@ -56,8 +61,11 @@ function createGrid(){
         clearCanvas();
     }
 
+    // Find the new corner pixels
     corner_List.push(gridSize, Math.pow(gridSize, 2) - gridSize + 1);
 
+    // Add pixels if grid size is increased
+    // Add id num to find corner pixels and hover event to change color
     if (gridSize >= curr_Size){
         grid_Diff = Math.pow(gridSize,2) - Math.pow(curr_Size,2);
         for (let i = 0; i < grid_Diff; i++) {
@@ -70,6 +78,7 @@ function createGrid(){
             div_Num += 1;
         }
     }
+    // Remove pixels if grid size is decreased
     else {
         grid_Diff = Math.pow(curr_Size,2) - Math.pow(gridSize,2);
         for (let i = 0; i < grid_Diff; i++) {
@@ -78,6 +87,7 @@ function createGrid(){
         }
     }
 
+    // Style newlyfound corners
     top_Right = document.getElementById(String(corner_List[0]));
     bottom_Left = document.getElementById(String(corner_List[1]));
     top_Right.style.cssText = 'border-top-right-radius: 1em;';
@@ -91,6 +101,9 @@ function sleep(ms) {
     });
 }
 
+// Shake only main section
+// Promise/sleep needed to remove classList or else adding/removing
+    // will be synchronous
 async function shakeCanvas(){
     const page = document.getElementsByTagName("main")[0];
     page.classList.add('shaking');
@@ -101,6 +114,8 @@ async function shakeCanvas(){
 
 function sketchStart() {
     createGrid();
+
+    // Change label number while dragging
     slider.addEventListener("input", () => {
         const gridSize = slider.value;
         slider_Label.textContent = gridSize;
@@ -108,6 +123,7 @@ function sketchStart() {
     slider.addEventListener("change", createGrid);
     buttons.forEach(button => {
         button.addEventListener('mousedown', () => {
+            // Clear seperated from buttons or else activated on hover
             if (button.id === "clear") { 
                 shakeCanvas();
             }
